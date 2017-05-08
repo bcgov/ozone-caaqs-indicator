4848c139b0d4309d63939f1a7f3c7dee6129ebc4
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-05-08 16:07:13 -0700

    rename raw data object

--- 01_load.R
+++ 01_load.R
@@ -27,10 +27,10 @@ unzip(file.path(path,ozone_zip), exdir = path)
 ## Load stations from file
 stations <- read.csv(file.path(path, stn_file), stringsAsFactors = FALSE)
 
-ozone <- read_csv(file.path(path, "O3_hourly.csv"), col_types = "cccccidc")
+ozone_all <- read_csv(file.path(path, "O3_hourly.csv"), col_types = "cccccidc")
 
 dir.create("tmp", showWarnings = FALSE)
-save(ozone, stations, file = "tmp/ozone_raw.RData")
+save(ozone_all, stations, file = "tmp/ozone_raw.RData")
 
 #####################################################
 # ## Some sanity checks
