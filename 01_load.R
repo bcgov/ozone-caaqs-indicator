0ab2993c8ba8d9658f9d0951006ebb0484ba0353
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-05-09 12:24:09 -0700

    sourcing station file from unverified data record

--- 01_load.R
+++ 01_load.R
@@ -14,7 +14,8 @@ library(readr)
 ## Download the ozone and station data from BC Data Catalogue:
 ## http://catalogue.data.gov.bc.ca/dataset/air-quality-monitoring-raw-hourly-data-and-station-data
 databc_ozone <- "http://pub.data.gov.bc.ca/datasets/77eeadf4-0c19-48bf-a47a-fa9eef01f409/O3_hourly.zip"
-databc_stations <- "http://catalogue.data.gov.bc.ca/dataset/77eeadf4-0c19-48bf-a47a-fa9eef01f409/resource/b833311d-4126-4dad-b57e-c9ce2c1133c2/download/bcairmonitoringstations.csv"
+#databc_stations <- "http://catalogue.data.gov.bc.ca/dataset/77eeadf4-0c19-48bf-a47a-fa9eef01f409/resource/b833311d-4126-4dad-b57e-c9ce2c1133c2/download/bcairmonitoringstations.csv"
+databc_stations <- "ftp://ftp.env.gov.bc.ca/pub/outgoing/AIR/Air_Monitoring_Stations/bc_air_monitoring_stations.csv"
 path <- "data"
 ozone_zip <- "O3_hourly.zip"
 stn_file <- "bc_air_monitoring_stations.csv"
@@ -61,14 +62,19 @@ save(ozone_all, stations, file = "tmp/ozone_raw.RData")
 # ggplot(ozone, aes(x = value)) + facet_wrap(~ site) + geom_histogram()
 # 
 # ## Which sites in ozone dataset don't have a corresponding site in ems locations
-# missing_sites <- unique(ozone$site)[!unique(ozone$ems_id) %in% stations$ems_id]
+# missing_sites <- unique(ozone$site)[!unique(ozone$ems_id) %in% stations$EMS_ID]
 # missing_sites
+# #Chetwynd SW BCOGC MAML missing from station file. Very little O# data so no issue for this analysis
 # 
-# #some of the ozone$ems_id's start with 0, which are missing in the statsions$ems-id versions
+# # some of the ozone$ems_id's start with 0, which are missing in the stations$ems-id versions when sourcing the station 
+# # file from the verified data record. issue resolved when chanegd to use station file in the unverified data record.
 # 
 # ## look at the station locations
-# station_points <- filter(stations, stations$Latitude != "NA", stations$ems_id != "E269223") #filter out NAs & error in ems_id == E269223
-# coordinates(station_points) <- c("Longitude", "Latitude") #use sp to tell R the lat/lon columns
+# station_points <- stations
+# station_points$LONGITUDE <- as.double(station_points$LONGITUDE)
+# station_points$LATITUDE <- as.double(station_points$LATITUDE)
+# station_points <- filter(station_points, station_points$LATITUDE != "NA") #filter out NAs
+# coordinates(station_points) <- c("LONGITUDE", "LATITUDE") #use sp to tell R the lat/lon columns
 # proj4string(station_points) <- "+init=epsg:4269" # projection of lat/longs
 # station_points <- spTransform(station_points, CRSobj = proj4string(airzones)) #match projection to bcmap layers
 # 
@@ -76,15 +82,3 @@ save(ozone_all, stations, file = "tmp/ozone_raw.RData")
 # plot(airzones) #from bcmaps
 # plot(station_points, add=TRUE)
 # 
-# 
-# # find_site <- function(re) {
-# #    i <- grep(re, ems_sites$MNTRNGLCT1[ems_sites$LCTNTPCD == "01"],
-# #              ignore.case = TRUE)
-# #    ems_sites[i,]
-# #  }
-# # 
-# # sapply(missing_sites, find_site, USE.NAMES = TRUE, simplify = FALSE)
-# # ## Most okay to be missing except for Port Moody Rocky Point Park
-# # 
-# # find_site("moody")
-
