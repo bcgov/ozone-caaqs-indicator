8b15942396fc2c9bbd44ea6a36100a45ed4bb891
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-05-10 15:44:39 -0700

    removed unused packages

--- 03_analysis.R
+++ 03_analysis.R
@@ -10,9 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-#library("openair")
-library("rcaaqs") # caaqs functions
-#library("lubridate")
+library("rcaaqs") # rcaaqs functions
 library("sp") # converting to spatial dataframe
 library("bcmaps") #air zone map
 library("dplyr") # for glimpse()
