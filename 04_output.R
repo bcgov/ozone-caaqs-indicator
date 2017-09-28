943f636f84f0d0184d6ecfcebb7faf10a7bc1a31
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-09-28 15:42:38 -0700

    one more try for size

--- 04_output.R
+++ 04_output.R
@@ -281,7 +281,7 @@ plot(mgmt_map)
 ## @knitr mgmt_map_end
 
 ## SVG of airzone CAAQS ambient achievement map
-svg_px("out/ozone_caaqs_mgmt_map.svg", width = 400, height = 400)
+svg_px("out/ozone_caaqs_mgmt_map.svg", width = 500, height = 500)
 plot(mgmt_map)
 dev.off()
 
@@ -332,7 +332,7 @@ mgmt_chart <- ggplot(data=ml_station.points,
 plot(mgmt_chart)
 
 ## SVG of airzone CAAQS ambient achievement bar chart
-svg_px("out/ozone_caaqs_mgmt_chart.svg", width = 400, height = 400)
+svg_px("out/ozone_caaqs_mgmt_chart.svg", width = 500, height = 500)
 plot(mgmt_chart)
 dev.off()
 
