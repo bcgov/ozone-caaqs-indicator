3db6f5244e675808fb541033c93e7ef98f3fdc73
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-12-03 15:13:16 -0800

    rename objects

--- 03_analysis.R
+++ 03_analysis.R
@@ -12,16 +12,15 @@
 
 library(rcaaqs) #rcaaqs functions
 library(dplyr) #data munging
-library(bcmaps) #airzone map
 library(lubridate) #wrangling dates
 
 
 ## Load the tmp file if ozone doesn't exist
-if (!exists("ozone")) load("tmp/ozone_clean.RData")
+if (!exists("ozone_df")) load("tmp/ozone_clean.RData")
 
 
 ## Ambient Ozone CAAQS analysis 
-ozone_caaqs <- o3_caaqs(ozone, by = c("ems_id", "station_name"))
+ozone_caaqs <- o3_caaqs(ozone_clean_data, by = c("ems_id", "station_name"))
 
 #look at ambient-only caaqs results
 ozone_caaqs_ambient_df <- get_caaqs(ozone_caaqs)
@@ -46,7 +45,8 @@ exclusions <- get_daily(ozone_caaqs) %>%
 
 
 ## Add management Ozone CAAQS analysis to ambient
-ozone_caaqs_mgmt <- caaqs_management(ozone_caaqs, exclude_df = exclusions, exclude_df_dt = "date")
+ozone_caaqs_mgmt <- caaqs_management(ozone_caaqs, exclude_df = exclusions,
+                                     exclude_df_dt = "date")
 
 #look at ambient+mgmt caaqs results
 ozone_caaqs_mgmt_df <- get_caaqs(ozone_caaqs_mgmt)
@@ -60,36 +60,32 @@ ozone_caaqs_df <- ozone_caaqs_mgmt_df %>%
          n_years > 1) 
 
 
-## Add info from stations_clean (created in 02.clean.R) & drop some columns
+## Add info from stations_az (created in 02.clean.R) & drop some columns
 ozone_caaqs_results <- ozone_caaqs_df %>% 
-  left_join(stations_clean, by = c("ems_id", "station_name")) %>% 
-  select(c(ems_id, station_name, city, longitude, latitude, metric,
-           caaqs_year, min_year,  max_year, n_years, ambient_metric_value, 
-           ambient_caaqs, excluded, mgmt_metric_value, mgmt_level,
+  left_join(stations_az, by = c("ems_id", "station_name")) %>% 
+  select(c(ems_id, station_name, city, airzone, metric,
+           caaqs_year, min_year,  max_year, n_years, metric_value_ambient, 
+           caaqs_ambient, excluded, metric_value_mgmt, mgmt_level,
            based_on_incomplete = flag_yearly_incomplete)) 
 
 
-## Assign airzone for each station
-
-#get airzone map (sf object) from bcmaps package
-az <- bcmaps::airzones()
-
-ozone_stn_az <- assign_airzone(ozone_caaqs_results, airzones = az,
-                               coords = c("longitude", "latitude"))
-
 ## Get airzone caaqs metric
-ozone_az <- airzone_metric(ozone_stn_az)
+ozone_az <- airzone_metric(ozone_caaqs_results)
 
 
 ## Save Ozone CAAQS & Management CAAQS objects
 save(ozone_caaqs, exclusions, ozone_caaqs_mgmt, ozone_caaqs_results,
-     ozone_stn_az, ozone_az, file = "tmp/analysed.RData")
+     ozone_az, file = "tmp/analysed.RData")
 
 
 ## Output Resources for the B.C. Data Catalogue
 
 #output results as CSV format
-write.csv(ozone_stn_az, "tmp/ozone_site_summary_2017.csv", row.names = FALSE)
+write.csv(ozone_caaqs_results, "tmp/ozone_site_summary_2017.csv", row.names = FALSE)
+
+ozone_az %>% 
+  left_join(az, by = c("Airzone" = "airzone")) %>% 
+  mutate(caaqs_ambient = replace_na(caaqs_ambient, "Insufficient Data"))
 write.csv(ozone_az, "tmp/ozone_airzone_summary_2017.csv", row.names = FALSE)
 
 
