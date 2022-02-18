0a8fa12eff0c7d91e52e7a9549a6dac13a2274d5
steffilazerte <steffi@steffi.ca>
2022-02-17 18:44:44 -0600

    Update analysis, (pm2.5 as template)

--- 03_analysis.R
+++ 03_analysis.R
@@ -1,92 +1,76 @@
 # Copyright 2015 Province of British Columbia
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
-library(rcaaqs) #rcaaqs functions
-library(dplyr) #data munging
-library(lubridate) #wrangling dates
+source("00_setup.R")
 
-## Load the tmp file if ozone doesn't exist
-if (!exists("ozone_clean_data")) load("tmp/ozone_clean.RData")
+library("readr")
+library("dplyr")
+library("tidyr")
+library("purrr")
+library("lubridate")
+library("assertr")
 
-## Ambient Ozone CAAQS analysis 
-ozone_caaqs <- o3_caaqs(ozone_clean_data, by = c("ems_id", "station_name"))
-
-#look at ambient-only caaqs results
-ozone_caaqs_ambient_df <- get_caaqs(ozone_caaqs)
-
-## Create Exceptional Events (EEs) and Transboundary Flows (TFs) dataframe for 
-## determining AQMS Air Zone Management Levels
-
-## Agassiz Municipal Hall (E293810): Two days in 2015 were flagged as exceptional 
-## events -- July 8 and 9 (2015)
-## Hope Airport (E223756): Three days in 2015 were flagged as exceptional 
-## events -- July 6, 8 and 9 (2015)
-## Hope Airport (E223756): Five days in 2017 were flagged as exceptional 
-## events -- August 3, 7, 10, 11, 29 (2017)
-## Hope Airport (E223756) : Seven days in 2018 were flagged as exceptional 
-## events -- July 27, 28, 29, 30 (2018) and August 8, 9, 22 (2018)
-## Mission School Works Yard (E302130) : six days in 2018 were flagges as exceptional 
-## events -- July 27, 29, 30, (2018) and August 8, 21, 22 (2018). 
+library("rcaaqs")
 
 
-## These days are removed (as a result of suspected wildfire influence) 
-## for determining AQMS Air Zone Management Levels
+# Load Data ----------------------
+ozone_clean <- read_rds("data/datasets/ozone_clean.rds")
+stations_clean <- read_rds("data/datasets/stations_clean.rds")
 
-#exclusion_dates_E293810 <- c("2015-07-08", "2015-07-09")
+# Transboundary Flows and Exceptional Events ------------------------------
+tfee <- ozone_clean %>%
+  filter(flag_tfee) %>%
+  # Ceiling TFEE to capture original dates
+  mutate(date = ceiling_date(date_time, unit = "hour"),
+         date = as_date(date)) %>%
+  select(site, date) %>%
+  distinct()
 
-exclusion_dates_E223756 <- c("2017-08-03", "2017-08-07", "2017-08-10", "2017-08-11", "2017-08-29", 
-                             "2018-07-27", "2018-07-28", "2018-07-29", "2018-07-30", "2018-08-08",
-                             "2018-08-09", "2018-08-22") 
-exclusion_dates_E302130 <- c("2018-07-27", "2018-07-29", "2018-07-30",  "2018-08-08", "2018-08-21", 
-                             "2018-08-22")
+# Calculate CAAQs --------------------------------------
 
-
-exclusions <- get_daily(ozone_caaqs) %>% 
-  filter((ems_id == "E223756" & date %in% as_date(exclusion_dates_E223756)) |
-              (ems_id == "E302130" & date %in% as_date(exclusion_dates_E302130))) %>% 
-  select(ems_id, station_name, date)
-
-
-## Add management Ozone CAAQS analysis to ambient
-ozone_caaqs_mgmt <- caaqs_management(ozone_caaqs, exclude_df = exclusions,
-                                     exclude_df_dt = "date")
-
-#look at ambient+mgmt caaqs results
-ozone_caaqs_mgmt_df <- get_caaqs(ozone_caaqs_mgmt)
+ozone_caaqs <- o3_caaqs(ozone_clean, by = "site")
+ozone_mgmt <- caaqs_management(ozone_caaqs, 
+                               exclude_df = tfee, 
+                               exclude_df_dt = "date")
 
 
-## Ozone CAAQS results for reporting year based on three year average
-ozone_caaqs_df <- ozone_caaqs_mgmt_df %>% 
-  group_by(ems_id) %>%
-  filter(caaqs_year == !!max_year, n_years > 1) 
-
-
-## Add info from stations_az (created in 02.clean.R) & drop some columns
-ozone_caaqs_results <- ozone_caaqs_df %>% 
-  left_join(stations_az, by = c("ems_id", "station_name")) %>% 
-  select(c(ems_id, station_name, city, lat, lon, airzone, metric,
-           caaqs_year, min_year,  max_year, n_years, metric_value_ambient, 
-           caaqs_ambient, excluded, metric_value_mgmt, mgmt_level,
-           based_on_incomplete = flag_yearly_incomplete))  %>% 
-  ungroup()
-
-## Get airzone caaqs metric
-ozone_az <- airzone_metric(ozone_caaqs_results)
-
-
-## Save Ozone CAAQS & Management CAAQS objects
-save(ozone_caaqs, exclusions, ozone_caaqs_mgmt, ozone_caaqs_results,
-     ozone_az, file = "tmp/analysed.RData")
+# Station results -----------------------------------------
+# Combine and filter
+ozone_results <- get_caaqs(ozone_mgmt) %>%
+  filter(caaqs_year == .env$rep_year, n_years > 1) %>% 
+  left_join(stations_clean, by = "site") %>% 
+  select(airzone, site, region, lat, lon, everything())
 
 
 
+# Airzone results ---------------------------------------------------------
+az_ambient <- airzone_metric(ozone_results, keep = c("site", "metric"), station_id = "site")
 
+az_mgmt <- az_ambient %>% 
+  group_by(airzone) %>%
+  slice(which.max(mgmt_level)) %>% 
+  mutate(caaqs_year = .env$rep_year) %>% 
+  ungroup() %>%
+  select(caaqs_year, airzone, mgmt_level, rep_metric = metric_ambient, 
+         metric_value = metric_value_mgmt, 
+         rep_stn_id = rep_stn_id_mgmt)
+
+write_rds(ozone_results, "data/datasets/ozone_results.rds")
+write_rds(az_ambient, "data/datasets/az_ambient.rds")
+write_rds(az_mgmt, "data/datasets/az_mgmt.rds")
+write_rds(ozone_mgmt, "data/datasets/ozone_mgmt.rds")
+
+write_csv(ozone_results, "out/ozone_caaqs_results.csv", na = "")
+write_csv(az_ambient, "out/ozone_airzone_results.csv" , na = "")
+write_csv(az_mgmt, "out/ozone_airzone_management_levels.csv", na = "")
