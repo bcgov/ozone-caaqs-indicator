a1a1036d887e5fcbd74fbb553d7309655adf1ed5
steffilazerte <steffi@steffi.ca>
2022-02-19 15:25:31 -0600

    Get tfee data for print

--- 03_analysis.R
+++ 03_analysis.R
@@ -29,7 +29,7 @@ ozone_clean <- read_rds("data/datasets/ozone_clean.rds")
 stations_clean <- read_rds("data/datasets/stations_clean.rds")
 
 # Transboundary Flows and Exceptional Events ------------------------------
-tfee <- ozone_clean %>%
+tfee_dates <- ozone_clean %>%
   filter(flag_tfee) %>%
   # Ceiling TFEE to capture original dates
   mutate(date = ceiling_date(date_time, unit = "hour"),
@@ -41,7 +41,7 @@ tfee <- ozone_clean %>%
 
 ozone_caaqs <- o3_caaqs(ozone_clean, by = "site")
 ozone_mgmt <- caaqs_management(ozone_caaqs, 
-                               exclude_df = tfee, 
+                               exclude_df = tfee_dates, 
                                exclude_df_dt = "date")
 
 
@@ -52,25 +52,45 @@ ozone_results <- get_caaqs(ozone_mgmt) %>%
   left_join(stations_clean, by = "site") %>% 
   select(airzone, site, region, lat, lon, everything())
 
-
-
 # Airzone results ---------------------------------------------------------
-az_ambient <- airzone_metric(ozone_results, keep = c("site", "metric"), station_id = "site")
+az_ambient <- airzone_metric(ozone_results, keep = c("site", "metric"), station_id = "site") %>%
+  select(-metric_mgmt, metric = metric_ambient, everything())
 
 az_mgmt <- az_ambient %>% 
   group_by(airzone) %>%
   slice(which.max(mgmt_level)) %>% 
   mutate(caaqs_year = .env$rep_year) %>% 
   ungroup() %>%
-  select(caaqs_year, airzone, mgmt_level, rep_metric = metric_ambient, 
+  select(caaqs_year, airzone, mgmt_level, rep_metric = metric, 
          metric_value = metric_value_mgmt, 
          rep_stn_id = rep_stn_id_mgmt)
 
+# For print version --------------------------------------------------------
+# Get reporting period tfee numbers for print version
+print_tfee <- get_daily(ozone_caaqs) %>% 
+  ungroup() %>%
+  filter(exceed, year(date) >= rep_year - 2) %>% 
+  semi_join(tfee_dates, by = c("site", "date")) %>%
+  arrange(date) %>%
+  group_by(year = year(date)) %>%
+  mutate(n_year = n_distinct(date)) %>%
+  ungroup() %>%
+  mutate(n_tfee_days = n_distinct(date)) %>%
+  pivot_wider(names_from = year, values_from = n_year, names_prefix = "n_tfee_days_") %>%
+  summarize(n_sites = n_distinct(site),
+            tfee_occurred_in_months = paste0(sort(unique(month(date))), collapse = ", "),
+            n_tfee_days = unique(n_tfee_days),
+            across(starts_with("n_tfee_days_"), .fns = ~na.omit(unique(.))))
+
+
+# Save next steps ----------------------------------------
 write_rds(ozone_results, "data/datasets/ozone_results.rds")
 write_rds(az_ambient, "data/datasets/az_ambient.rds")
 write_rds(az_mgmt, "data/datasets/az_mgmt.rds")
 write_rds(ozone_mgmt, "data/datasets/ozone_mgmt.rds")
+write_rds(print_tfee, "data/datasets/print_tfee.rds")
 
+# Save csvs --------------------------------------------------------
 write_csv(ozone_results, "out/ozone_caaqs_results.csv", na = "")
 write_csv(az_ambient, "out/ozone_airzone_results.csv" , na = "")
 write_csv(az_mgmt, "out/ozone_airzone_management_levels.csv", na = "")
